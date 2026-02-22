7e24116fb973546b38032c9cf9304e6afd46ffd6
Danny <dlkrupczak@gmail.com>
2026-02-22 07:15:36 -0500

    Basic Menu and Level

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void BacktoMain()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
