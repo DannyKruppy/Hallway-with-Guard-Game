b5e30db5995cb5c9163697bed99d1f6e3fff6546
nominal251 <jackhardy524@gmail.com>
2026-02-25 15:51:52 -0500

    Gun part 1
    implemented a working gun with floaty rotation and a simple test raycast

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -1,23 +1,63 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class GunScript : MonoBehaviour
 {
     public Transform target;
+    public Transform adsTarget;
     public Transform recoilObject;
     public float followSpeed = 20f;
     public float rotationFollowSpeed = 1f;
 
-    void LateUpdate()
+    public Transform raycastOrigin;
+    public GameObject hitEffect;
+
+    public int ammo = 60;
+
+    public TextMeshProUGUI ammoText;
+
+    void Start()
+    {
+        ammoText.text = ("Ammo: " + ammo);
+    }
+
+    void Update()
     {
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
+            FireWeapon();
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (Mouse.current.rightButton.isPressed)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, adsTarget.localPosition, followSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, adsTarget.rotation, rotationFollowSpeed * Time.deltaTime);
+        } else
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, target.localPosition, followSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, rotationFollowSpeed * Time.deltaTime);
+        }
+    }
+
+    void FireWeapon()
+    {
+        if (ammo > 0)
+        {
+            ammo--;
+            ammoText.text = ("Ammo: " + ammo);
+
+            RaycastHit hit;
+            if (Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hit, 1000))
+            {
+                Instantiate(hitEffect, hit.point, Quaternion.identity);
+            }
+
             transform.position = recoilObject.position;
             transform.rotation = recoilObject.rotation;
         }
-
-        transform.position = Vector3.Lerp(transform.position, target.position, followSpeed * Time.deltaTime);
-
-        transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, rotationFollowSpeed * Time.deltaTime);
     }
 }
