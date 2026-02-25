23026222500b8cac02689e44b5873e42f8e4bf29
nominal251 <jackhardy524@gmail.com>
2026-02-24 23:47:07 -0500

    Implement recoil

---
+++ Assets/Scripts/GunWeight.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GunWeight : MonoBehaviour
+{
+    public Transform target;
+    public Transform recoilObject;
+    public float followSpeed = 20f;
+    public float rotationFollowSpeed = 1f;
+
+    void LateUpdate()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            transform.position = recoilObject.position;
+            transform.rotation = recoilObject.rotation;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, target.position, followSpeed * Time.deltaTime);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, rotationFollowSpeed * Time.deltaTime);
+    }
+}
