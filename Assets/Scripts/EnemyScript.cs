b4190d0ccc42f9cbd044e99ec005496afa90d212
nominal251 <jackhardy524@gmail.com>
2026-03-03 11:20:00 -0500

    Add waypoint navigation

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,19 +1,33 @@
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections.Generic;
 
 public class EnemyScript : MonoBehaviour
 {
 
-    public Transform goal;
+    public List<Transform> waypoints;
+
     private NavMeshAgent agent;
+    private int index = 0;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        NextWaypoint();
     }
 
     void Update()
     {
-        agent.destination = goal.position;
+        if (!agent.pathPending && agent.remainingDistance < agent.stoppingDistance)
+        {
+            NextWaypoint();
+        }
+    }
+
+    void NextWaypoint()
+    {
+        agent.destination = waypoints[index].position;
+        index = (index + 1) % waypoints.Count;
     }
 }
