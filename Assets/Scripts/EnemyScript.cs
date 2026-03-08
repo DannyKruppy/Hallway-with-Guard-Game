9567f3419eb2266dfeb947e9fe78c3e760dba82d
nominal251 <jackhardy524@gmail.com>
2026-03-07 19:48:33 -0500

    Add suspicion meter

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -28,6 +28,13 @@ public class EnemyScript : MonoBehaviour
     private bool searching = false;
     private bool hadLineOfSight = false;
 
+    public float suspicion = 0f;
+    public float suspicionIncreaseRate = 80f; // per second when player visible
+    public float suspicionDecreaseRate = 20f; // per second when player not visible
+    public float suspicionMax = 100f;
+    public float instantAggroRange = 2f;
+    public TextMeshProUGUI suspicionText;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -39,26 +46,41 @@ public class EnemyScript : MonoBehaviour
         Vector3 direction = rayReceiver.position - raySender.position;
         RaycastHit hit;
 
-        if (Physics.Raycast(raySender.position, direction.normalized, out hit, direction.magnitude) && hit.collider.CompareTag("Player") && playerInCone)
+        suspicion = Mathf.Clamp(suspicion, 0f, 100f);
+        suspicionText.text = ("Suspicion %: " + suspicion);
+
+        if (direction.magnitude <= instantAggroRange)
         {
-            losText.text = "In line of sight";
-            hadLineOfSight = true;
-
-            agent.destination = player.position;
-            agent.stoppingDistance = aggroStoppingDistance;
-
-            Vector3 lookDir = player.position - transform.position;
-            lookDir.y = 0;
-            transform.rotation = Quaternion.LookRotation(lookDir);
+            suspicion = 100f;
+        } 
+        else if (Physics.Raycast(raySender.position, direction.normalized, out hit, direction.magnitude) && hit.collider.CompareTag("Player") && playerInCone)
+        {
+            suspicion += suspicionIncreaseRate * Time.deltaTime;
         }
         else
         {
             losText.text = "";
+            suspicion -= suspicionDecreaseRate * Time.deltaTime;
 
             if (hadLineOfSight && !searching)
             {
                 StartCoroutine(SearchForPlayer());
             }
+        }
+
+        if (suspicion >= suspicionMax)
+        {
+            losText.text = "In line of sight";
+            hadLineOfSight = true;
+
+            //set player as movement target and increase stoppin distnce
+            agent.destination = player.position;
+            agent.stoppingDistance = aggroStoppingDistance;
+
+            //constantly rotate enemy towards player
+            Vector3 lookDir = player.position - transform.position;
+            lookDir.y = 0;
+            transform.rotation = Quaternion.LookRotation(lookDir);
         }
     }
 
